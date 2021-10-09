d98b2fc5eb843680a15ff455a54b813e5b53d69d
FrankZRS <hzzhongruoshui@hotmail.com>
2021-10-09 10:19:21 +0100

    Rewrote cn symbols loader

--- cn_symbols_modifier.py
+++ cn_symbols_modifier.py
@@ -15,3 +15,46 @@ for symbol in cn_symbols:
 with open("cn_symbols.txt", "w") as file: 
     for symbol in modified_symbols: 
         file.write(f"{symbol}\n")
+
+# Old cn_symbols loader
+# if market == "cn" or market == "all": 
+#         SS_A = 600000 # 沪A
+#         SZ_A = 0      # 深A
+#         CYB = 300000  # 创业板
+#         ZXB = 2000    # 中小板
+#         SS_B = 900000 # 沪B
+#         SZ_B = 200000 # 深B
+
+#         # 沪A
+#         while SS_A <= 601999: 
+#             symbols.append(f"{SS_A:06d}.SS")
+#             SS_A += 1
+
+#         SS_A = 603000
+#         while SS_A <= 603999: 
+#             symbols.append(f"{SS_A:06d}.SS")
+#             SS_A += 1
+
+#         SS_A = 605000
+#         while SS_A <= 605999: 
+#             symbols.append(f"{SS_A:06d}.SS")
+#             SS_A += 1
+        
+#         # 深A
+#         while SZ_A <= 999: 
+#             symbols.append(f"{SZ_A:06d}.SZ")
+#             SZ_A += 1
+
+#         symbols.append("001696")
+#         symbols.append("001896")
+#         symbols.append("001979")
+
+#         # 创业板
+#         while CYB <= 301200: 
+#             symbols.append(f"{CYB:06d}.SZ")
+#             CYB += 1
+
+#         # 中小板
+#         while ZXB <= 2999: 
+#             symbols.append(f"{ZXB:06d}.SZ")
+#             ZXB += 1
--- stock.py
+++ stock.py
@@ -31,46 +31,9 @@ def load_symbols(market):
             symbols.extend(us_symbols)
 
     if market == "cn" or market == "all": 
-        SS_A = 600000 # 沪A
-        SZ_A = 0      # 深A
-        CYB = 300000  # 创业板
-        ZXB = 2000    # 中小板
-        SS_B = 900000 # 沪B
-        SZ_B = 200000 # 深B
-
-        # 沪A
-        while SS_A <= 601999: 
-            symbols.append(f"{SS_A:06d}.SS")
-            SS_A += 1
-
-        SS_A = 603000
-        while SS_A <= 603999: 
-            symbols.append(f"{SS_A:06d}.SS")
-            SS_A += 1
-
-        SS_A = 605000
-        while SS_A <= 605999: 
-            symbols.append(f"{SS_A:06d}.SS")
-            SS_A += 1
-        
-        # 深A
-        while SZ_A <= 999: 
-            symbols.append(f"{SZ_A:06d}.SZ")
-            SZ_A += 1
-
-        symbols.append("001696")
-        symbols.append("001896")
-        symbols.append("001979")
-
-        # 创业板
-        while CYB <= 301200: 
-            symbols.append(f"{CYB:06d}.SZ")
-            CYB += 1
-
-        # 中小板
-        while ZXB <= 2999: 
-            symbols.append(f"{ZXB:06d}.SZ")
-            ZXB += 1
+        with open("cn_symbols.txt", "r") as file: 
+            cn_symbols = file.read().splitlines()
+            symbols.extend(cn_symbols)
     return symbols
 
 def check_market(market): 
@@ -107,10 +70,10 @@ def check_market_cap(currency, market_cap):
             return True
         return False
 
-    if currency == "CNY": 
-        if market_cap > 200000000000: 
-            return True
-        return False
+    # if currency == "CNY": 
+    #     if market_cap > 10000000000: 
+    #         return True
+    #     return False
     return False
 
 def read_single_candle(candle): 
