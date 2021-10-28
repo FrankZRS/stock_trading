1f04ee235f41180aff28627d41252db17e23cf3b
Frank Zhong <f.zhong@cascoda.com>
2021-10-28 10:35:42 +0100

    Filtered us stocks

--- symbols_filter.py
+++ symbols_filter.py
@@ -1,31 +1,35 @@
 import yfinance as yf
 from stock import check_market_cap
 
-with open("symbols_gb.txt", "r+") as file1: 
-    gb_symbols = file1.read().splitlines()
+symbol_files = ["symbols_gb.txt", "symbols_us.txt"]
+for symbol_file in symbol_files: 
+    if symbol_file == "symbols_us.txt": 
+        with open(symbol_file, "r+") as file1: 
+            symbols = file1.read().splitlines()
 
-    for gb_symbol in gb_symbols: 
-        try: 
-            stock = yf.Ticker(gb_symbol)
+            for symbol in symbols: 
+                try: 
+                    stock = yf.Ticker(symbol)
 
-            market = stock.info['market']
+                    market = stock.info['market']
 
-            if not check_market_cap(stock.info["financialCurrency"], stock.info["marketCap"]): 
-                print(f"Small cap for {gb_symbol}")
-                
-                gb_symbols.remove(gb_symbol)
+                    if not check_market_cap(stock.info["financialCurrency"], stock.info["marketCap"]): 
+                        print(f"Small cap for {symbol}")
+                        
+                        symbols.remove(symbol)
 
-                with open("symbols_small_cap.txt", "a") as file2: 
-                    file2.write(f"{gb_symbol}\n")
+                        with open("symbols_small_cap.txt", "a") as file2: 
+                            file2.write(f"{symbol}\n")
 
-        except Exception as e: 
-            print(f"Exception {e} for {gb_symbol}")
-                
-            gb_symbols.remove(gb_symbol)
+                except Exception as e: 
+                    print(f"Exception {e} for {symbol}")
+                        
+                    symbols.remove(symbol)
 
-            with open("symbols_info_error.txt", "a") as file3: 
-                    file3.write(f"{gb_symbol}\n")
+                    with open("symbols_info_error.txt", "a") as file3: 
+                            file3.write(f"{symbol}\n")
 
-    file1.seek(0)
-    for gb_symbol in gb_symbols: 
-            file1.write(f"{gb_symbol}\n")
+            file1.seek(0)
+            for symbol in symbols: 
+                    file1.write(f"{symbol}\n")
+                    file1.truncate()
