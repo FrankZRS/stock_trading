5b51b5b5965a8b2328d8d9e89ab0181af8f44890
Frank Zhong <hzzhongruoshui@hotmail.com>
2024-03-18 19:39:42 +0000

    Remove trailing spaces

--- stock.py
+++ stock.py
@@ -6,13 +6,13 @@ import os
 from datetime import datetime
 import webbrowser
 
-def enable_print(): 
+def enable_print():
     sys.stdout = sys.__stdout__
 
-def disable_print(): 
+def disable_print():
     sys.stdout = open(os.devnull, "w")
 
-def load_symbols(market): 
+def load_symbols(market):
     """
     Load stock symbols
 
@@ -21,23 +21,23 @@ def load_symbols(market):
 
     symbols = []
 
-    if market == "gb" or market == "all": 
-        with open("symbols_gb.txt", "r") as file: 
+    if market == "gb" or market == "all":
+        with open("symbols_gb.txt", "r") as file:
             gb_symbols = file.read().splitlines()
             symbols.extend(gb_symbols)
 
-    if market == "us" or market == "all": 
-        with open("symbols_us.txt", "r") as file: 
+    if market == "us" or market == "all":
+        with open("symbols_us.txt", "r") as file:
             us_symbols = file.read().splitlines()
             symbols.extend(us_symbols)
 
-    if market == "cn" or market == "all": 
-        with open("symbols_cn.txt", "r") as file: 
+    if market == "cn" or market == "all":
+        with open("symbols_cn.txt", "r") as file:
             cn_symbols = file.read().splitlines()
             symbols.extend(cn_symbols)
     return symbols
 
-def check_market_cap(currency, market_cap): 
+def check_market_cap(currency, market_cap):
     """
     Filter company size by its market cap
 
@@ -49,34 +49,34 @@ def check_market_cap(currency, market_cap):
     # print(market_cap)
     # disable_print()
 
-    if currency == "GBP": 
-        if market_cap > 5000000000: 
+    if currency == "GBP":
+        if market_cap > 5000000000:
             return True
         return False
 
-    if currency == "USD": 
-        if market_cap > 7000000000: 
+    if currency == "USD":
+        if market_cap > 7000000000:
             return True
         return False
 
-    if currency == "CNY": 
+    if currency == "CNY":
         return True
-        # if market_cap > 10000000000: 
+        # if market_cap > 10000000000:
         #     return True
         # return False
     return False
 
-def calculate_moving_average(data, days): 
+def calculate_moving_average(data, days):
     return data.tail(days).iloc[:,3].mean()
 
-def check_downtrend(data): 
+def check_downtrend(data):
     total_days = len(data.index)
     MA5 = []
     MA10 = []
     MA20 = []
     
     candle_count = total_days - 10
-    while candle_count < total_days: 
+    while candle_count < total_days:
         MA5.append(calculate_moving_average(data.iloc[ : candle_count + 1], 5))
         MA10.append(calculate_moving_average(data.iloc[ : candle_count + 1], 10))
         MA20.append(calculate_moving_average(data.iloc[ : candle_count + 1], 20))
@@ -84,20 +84,20 @@ def check_downtrend(data):
 
     checker_count = 0
     day_count = 0
-    while checker_count < 10: 
-        if MA20[checker_count] > MA10[checker_count] > MA5[checker_count]: 
+    while checker_count < 10:
+        if MA20[checker_count] > MA10[checker_count] > MA5[checker_count]:
             day_count += 1
-        else: 
+        else:
             day_count = 0
         
-        if day_count == 3: 
+        if day_count == 3:
             return True
         
         checker_count += 1
 
     return False
     
-def read_single_candle(data): 
+def read_single_candle(data):
     """
     Virtually create a candlestick
 
@@ -112,7 +112,7 @@ def read_single_candle(data):
     candle['close'] = data.iloc[0]['Close']
 
     candle['full_range'] = candle['high'] - candle['low']
-    if candle['full_range'] == 0: 
+    if candle['full_range'] == 0:
         return None
         
     candle['upper_shadow'] = candle['high'] - max(candle['open'], candle['close'])
@@ -120,79 +120,79 @@ def read_single_candle(data):
     candle['lower_shadow'] = min(candle['open'], candle['close']) - candle['low']
     return candle
 
-def check_hammer(stock, data): 
+def check_hammer(stock, data):
     """
     Check for hammer and inverted hammer
     
     锤，倒锤
     """
 
-    if not check_downtrend(data.tail(30)): 
+    if not check_downtrend(data.tail(30)):
         return False
 
     data = data.tail(1)
     candle = read_single_candle(data)
-    if candle == None: 
+    if candle == None:
         return False
 
-    if candle['lower_shadow'] > 2 * candle['body'] and candle['upper_shadow'] / candle['full_range'] <= 0.1: 
+    if candle['lower_shadow'] > 2 * candle['body'] and candle['upper_shadow'] / candle['full_range'] <= 0.1:
         enable_print()
         print(f"Hammer, {stock.info['symbol']} ({stock.info['shortName']}), {candle['date']}")
         disable_print()
-    elif candle['upper_shadow'] > 2 * candle['body'] and candle['lower_shadow'] / candle['full_range'] <= 0.1: 
+    elif candle['upper_shadow'] > 2 * candle['body'] and candle['lower_shadow'] / candle['full_range'] <= 0.1:
         enable_print()
         print(f"Inverted hammer, {stock.info['symbol']} ({stock.info['shortName']}), {candle['date']}")
         disable_print()
-    else: 
+    else:
         return False
     return True
 
-def check_doji(stock, data): 
+def check_doji(stock, data):
     """
     Check for doji, long-legged doji and spinning top
 
     十字星，长十字星，螺旋桨
     """
     
-    if not check_downtrend(data.tail(30)): 
+    if not check_downtrend(data.tail(30)):
         return False
 
     data = data.tail(1)
     candle = read_single_candle(data)
-    if candle == None: 
+    if candle == None:
         return False
     
-    if candle['upper_shadow'] / candle['full_range'] >= 0.3 and candle['lower_shadow'] / candle['full_range'] >= 0.3: 
-        if candle['body'] / candle['full_range'] <= 0.05: 
-            if candle['upper_shadow'] / candle['open'] >= 0.03 and candle['lower_shadow'] / candle['open'] >= 0.03: 
+    if candle['upper_shadow'] / candle['full_range'] >= 0.3 and candle['lower_shadow'] / candle['full_range'] >= 0.3:
+        if candle['body'] / candle['full_range'] <= 0.05:
+            if candle['upper_shadow'] / candle['open'] >= 0.03 and candle['lower_shadow'] / candle['open'] >= 0.03:
                 enable_print()
                 print(f"Long-legged doji, {stock.info['symbol']} ({stock.info['shortName']}), {candle['date']}")
                 disable_print()
-            else: 
+            else:
                 enable_print()
                 print(f"Doji, {stock.info['symbol']} ({stock.info['shortName']}), {candle['date']}")
                 disable_print()
-        elif candle['body'] / candle['full_range'] <= 0.3: 
+        elif candle['body'] / candle['full_range'] <= 0.3:
             enable_print()
             print(f"Spinning top, {stock.info['symbol']} ({stock.info['shortName']}), {candle['date']}")
             disable_print()
         return True
-    else: 
+    else:
         return False
 
-def check_marubozu(stock, data): 
+def check_marubozu(stock, data):
     """
     Check for marubozu
 
     光头光脚阳线
     """
     
-    if not check_downtrend(data.tail(30)): 
+    if not check_downtrend(data.tail(30)):
         return False
 
     data = data.tail(1)
     candle = read_single_candle(data)
-    if candle == None: 
+    if candle == None:
         return False
 
     short_upper_shadow = True if candle['upper_shadow'] / candle['full_range'] <= 0.05 else False
@@ -200,18 +200,18 @@ def check_marubozu(stock, data):
     long_body = True if candle['body'] / candle['full_range'] >= 0.7 else False
     soar = True if (candle['close'] - candle['open']) / candle['open'] >= 0.01 else False
     
-    if long_body and soar: 
-        if short_upper_shadow and short_lower_shadow: 
+    if long_body and soar:
+        if short_upper_shadow and short_lower_shadow:
             enable_print()
             print(f"Marubozu, {stock.info['symbol']} ({stock.info['shortName']}), {candle['date']}")
             disable_print()
             return True
-        elif short_upper_shadow: 
+        elif short_upper_shadow:
             enable_print()
             print(f"Marubozu closing, {stock.info['symbol']} ({stock.info['shortName']}), {candle['date']}")
             disable_print()
             return True
-        elif short_lower_shadow: 
+        elif short_lower_shadow:
             enable_print()
             print(f"Marubozu opening, {stock.info['symbol']} ({stock.info['shortName']}), {candle['date']}")
             disable_print()
@@ -219,14 +219,14 @@ def check_marubozu(stock, data):
     
     return False
 
-def check_engulfing(stock, data): 
+def check_engulfing(stock, data):
     """
     Check for bullish engulfing
 
     阳包阴
     """
     
-    if not check_downtrend(data.tail(30)): 
+    if not check_downtrend(data.tail(30)):
         return False
 
     data = data.tail(2)
@@ -234,26 +234,26 @@ def check_engulfing(stock, data):
     candles = []
 
     candle_count = 0
-    while candle_count < total_days: 
+    while candle_count < total_days:
         candles.append(read_single_candle(data.iloc[candle_count : candle_count + 1]))
         candle_count += 1
 
-    if candles[0]['close'] < candles[0]['open'] and candles[1]['close'] > candles[1]['open'] and candles[1]['open'] < candles[0]['close'] and candles[1]['close'] > candles[0]['open']: 
+    if candles[0]['close'] < candles[0]['open'] and candles[1]['close'] > candles[1]['open'] and candles[1]['open'] < candles[0]['close'] and candles[1]['close'] > candles[0]['open']:
         enable_print()
         print(f"Engulfing, {stock.info['symbol']} ({stock.info['shortName']}), {candles[0]['date']} ~ {candles[1]['date']}")
         disable_print()
         return True
-    else: 
+    else:
         return False
 
-def check_three_white_soldiers(stock, data): 
+def check_three_white_soldiers(stock, data):
     """
     Check for three white soldiers
 
     红三兵
     """
 
-    if not check_downtrend(data.tail(30)): 
+    if not check_downtrend(data.tail(30)):
         return False
 
     data = data.tail(3)
@@ -261,7 +261,7 @@ def check_three_white_soldiers(stock, data):
     candles = []
 
     candle_count = 0
-    while candle_count < total_days: 
+    while candle_count < total_days:
         candles.append(read_single_candle(data.iloc[candle_count : candle_count + 1]))
 
         candle_count += 1
@@ -269,9 +269,9 @@ def check_three_white_soldiers(stock, data):
     all_rise = True if candles[0]['close'] > candles[0]['open'] and candles[1]['close'] > candles[1]['open'] and candles[2]['close'] > candles[2]['open'] else False
     all_long_body = True if candles[0]['body'] / candles[0]['full_range'] >= 0.7 and candles[1]['body'] / candles[1]['full_range'] >= 0.7 and candles[2]['body'] / candles[2]['full_range'] >= 0.7 else False
 
-    if all_rise and all_long_body: 
-        for index in range(total_days - 1): 
-            if not (candles[index + 1]['close'] > candles[index]['high'] and candles[index]['open'] <= candles[index + 1]['open'] <= candles[index]['close']): 
+    if all_rise and all_long_body:
+        for index in range(total_days - 1):
+            if not (candles[index + 1]['close'] > candles[index]['high'] and candles[index]['open'] <= candles[index + 1]['open'] <= candles[index]['close']):
                 return False
         
         enable_print()
@@ -281,14 +281,14 @@ def check_three_white_soldiers(stock, data):
     
     return False
 
-def check_twin_needle(stock, data): 
+def check_twin_needle(stock, data):
     """
     Check for twin needle (bottom)
 
     双针探底
     """
     
-    if not check_downtrend(data.tail(30)): 
+    if not check_downtrend(data.tail(30)):
         return False
 
     data = data.tail(5)
@@ -297,7 +297,7 @@ def check_twin_needle(stock, data):
     lows = []
 
     candle_count = 0
-    while candle_count < total_days: 
+    while candle_count < total_days:
         candles.append(read_single_candle(data.iloc[candle_count : candle_count + 1]))
 
         lows.append(candles[candle_count]['low'])
@@ -313,15 +313,15 @@ def check_twin_needle(stock, data):
 
     all_long_candle = True if lowest_candle_1['full_range'] / lowest_candle_1['open'] >= 0.01 and lowest_candle_2['full_range'] / lowest_candle_2['open'] >= 0.01 else False
 
-    if abs(lowest_candle_1['low'] - lowest_candle_2['low']) / lowest_candle_2['close'] <= 0.003 and all_long_candle and lowest_candle_1['lower_shadow'] / lowest_candle_1['full_range'] >= 0.3 and lowest_candle_2['lower_shadow'] / lowest_candle_2['full_range'] >= 0.3: 
+    if abs(lowest_candle_1['low'] - lowest_candle_2['low']) / lowest_candle_2['close'] <= 0.003 and all_long_candle and lowest_candle_1['lower_shadow'] / lowest_candle_1['full_range'] >= 0.3 and lowest_candle_2['lower_shadow'] / lowest_candle_2['full_range'] >= 0.3:
         enable_print()
         print(f"Twin needle, {stock.info['symbol']} ({stock.info['shortName']}), {lowest_candle_1['date']} & {lowest_candle_2['date']}")
         disable_print()
         return True
-    else: 
+    else:
         return False
 
-def check_island(stock, data, max_days): 
+def check_island(stock, data, max_days):
     """
     Check for island reversal
 
@@ -334,7 +334,7 @@ def check_island(stock, data, max_days):
     lows = []
 
     candle_count = 0
-    while candle_count < total_days: 
+    while candle_count < total_days:
         candles.append(read_single_candle(data.iloc[candle_count : candle_count + 1]))
 
         highs.append(candles[candle_count]['high'])
@@ -343,7 +343,7 @@ def check_island(stock, data, max_days):
 
     # Save high and low prices into lists
     candle_count = 0
-    while candle_count < total_days: 
+    while candle_count < total_days:
         highs.append(data.iloc[candle_count]['High'])
         lows.append(data.iloc[candle_count]['Low'])
         candle_count += 1
@@ -351,21 +351,21 @@ def check_island(stock, data, max_days):
     day_count_start = 0
 
     # Check start of island
-    while day_count_start < total_days - 1: # No need to check the last day 
-        if lows[day_count_start] > highs[day_count_start + 1]: 
+    while day_count_start < total_days - 1: # No need to check the last day
+        if lows[day_count_start] > highs[day_count_start + 1]:
             day_count_end = day_count_start + 2
 
             # Check end of island
-            while day_count_end < total_days: 
-                if lows[day_count_start] > max(highs[day_count_start + 1: day_count_end + 1]): 
+            while day_count_end < total_days:
+                if lows[day_count_start] > max(highs[day_count_start + 1: day_count_end + 1]):
                     day_count_end += 1
 
-                    if day_count_end == total_days: 
+                    if day_count_end == total_days:
                         return False # This is a cliff (half island)
                 else: 
                     break # This is a potential end of island
 
-            if lows[day_count_end] > max(highs[day_count_start + 1: day_count_end]): 
+            if lows[day_count_end] > max(highs[day_count_start + 1: day_count_end]):
                 current_date = datetime.now()
 
                 delta = current_date - data.index[day_count_end]
@@ -381,26 +381,26 @@ def check_island(stock, data, max_days):
     
     return False # There is no island
         
-def main(): 
+def main():
     symbols = load_symbols("all")
     
-    for symbol in symbols: 
+    for symbol in symbols:
 
         # enable_print()
         # print(symbol)
         # disable_print()
 
-        try: 
+        try:
             disable_print()
 
             stock = yf.Ticker(symbol)
 
             # enable_print()
-            # for key in stock.info: 
+            # for key in stock.info:
             #     print(f"{key}: {stock.info[key]}\n")
             # disable_print()
 
-            if not check_market_cap(stock.info["financialCurrency"], stock.info["marketCap"]): 
+            if not check_market_cap(stock.info["financialCurrency"], stock.info["marketCap"]):
                 continue
 
             # get data of this stock
@@ -410,17 +410,17 @@ def main():
             # print(data.tail(1+1).iloc[:,3].pct_change())
             # disable_print()
 
-            result = any([check_hammer(stock, data), 
-                         check_doji(stock, data), 
-                         check_marubozu(stock, data), 
-                         check_engulfing(stock, data), 
-                         check_three_white_soldiers(stock, data), 
-                         check_twin_needle(stock, data), 
+            result = any([check_hammer(stock, data),
+                         check_doji(stock, data),
+                         check_marubozu(stock, data),
+                         check_engulfing(stock, data),
+                         check_three_white_soldiers(stock, data),
+                         check_twin_needle(stock, data),
                          check_island(stock, data, 3)])
             
-            # if result: 
-            #     webbrowser.open(f"https://uk.finance.yahoo.com/chart/{stock.info['symbol']}")
-        except Exception as e: 
+            if result:
+                webbrowser.open(f"https://uk.finance.yahoo.com/chart/{stock.info['symbol']}")
+        except Exception as e:
             pass
 
 if __name__ == "__main__":
